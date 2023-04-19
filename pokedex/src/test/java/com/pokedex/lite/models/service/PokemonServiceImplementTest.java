14f1ebbbe8ad002de97562211fa919bf640b4f98
Martín Ottaviano <mmottaviano@gmail.com>
2023-04-19 02:52:42 -0300

    Test Cases

--- pokedex/src/test/java/com/pokedex/lite/models/service/PokemonServiceImplementTest.java
+++ pokedex/src/test/java/com/pokedex/lite/models/service/PokemonServiceImplementTest.java
@@ -3,6 +3,8 @@ package com.pokedex.lite.models.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -23,15 +25,30 @@ public class PokemonServiceImplementTest {
 	
 	@Test
 	public void testNotNull() {
-		Pokemon pokemon = new Pokemon();
+		Pokemon pokemon = new Pokemon("Spheal", "Ice", "Water", 3, null,  null);
 		
 		assertNotNull(pokemon);
 	}
-		
+	
 	@Test
-	public void TestTU02FindbyName() {
-
+	public void testFindbyName() {
 		assertEquals(pokemonService.findByName("Lapras").getName(), "Lapras");
+	}
+	
+	@Test
+	public void testAddPokemon() {	
+		Pokemon pokemon = new Pokemon("Seel", "Ice", "Water", 3, null,  null);
+		pokemonService.save(pokemon);
+		assertEquals(pokemonService.findByName("Seel").getName(), "Seel");
+	}
+	
+	@Test
+	public void testUpdatePokemon() {
+		Pokemon pokemon = pokemonService.searchById(2L);
+		pokemon.setName("Slowpoke");
+		pokemonService.save(pokemon);
+
+		assertEquals(pokemonService.findByName("Slowpoke").getName(), "Slowpoke");
 	}
 	
 	
